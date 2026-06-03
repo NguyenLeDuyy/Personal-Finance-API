bde4cbe03168a8dda1398197dad8c3446eb513db
NguyenLeDuyy <nguyenleduy10122004@gmail.com>
2026-06-03 19:25:22 +0700

    fix: map RefreshToken entity to refresh_tokens table

--- src/main/java/com/taskmanagement/task_management_api/entity/RefreshToken.java
+++ src/main/java/com/taskmanagement/task_management_api/entity/RefreshToken.java
@@ -8,11 +8,13 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "refresh_tokens")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
